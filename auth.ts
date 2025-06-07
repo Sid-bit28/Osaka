2b69cd1e202d81508c61e4ad99454b9c198510eb
Siddarth Banerjee <siddarth2801@gmail.com>
2025-06-07 17:05:37 +0530

    added callbacks for next-auth

--- auth.ts
+++ auth.ts
@@ -1,9 +1,43 @@
-import NextAuth from 'next-auth';
+import NextAuth, { type DefaultSession } from 'next-auth';
 import authConfig from '@/auth.config';
 import { db } from './lib/db';
 import { PrismaAdapter } from '@auth/prisma-adapter';
+import { getUserById } from './data/user';
+
+// auth documentations of adapter types
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      role: 'ADMIN' | 'GUEST';
+    } & DefaultSession['user'];
+  }
+}
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role as 'ADMIN' | 'GUEST';
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) {
+        return token;
+      }
+
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) {
+        return token;
+      }
+
+      token.role = existingUser.role;
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authConfig,
