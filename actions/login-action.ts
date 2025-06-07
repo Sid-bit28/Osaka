fa6d1201eca773f9b737f156ac034ecdc4cd6a29
Siddarth Banerjee <siddarth2801@gmail.com>
2025-06-07 14:06:55 +0530

    bug fixing replaces bcrypt with bcryptjs

--- actions/login-action.ts
+++ actions/login-action.ts
@@ -2,6 +2,9 @@
 
 import * as z from 'zod';
 import { SignInSchema } from '@/schemas';
+import { signIn } from '@/auth';
+import { DEFAULT_LOGIN_REDIRECT } from '@/constants/routes';
+import { AuthError } from 'next-auth';
 
 export const loginAction = async (values: z.infer<typeof SignInSchema>) => {
   const validatedFields = SignInSchema.safeParse(values);
@@ -10,6 +13,32 @@ export const loginAction = async (values: z.infer<typeof SignInSchema>) => {
       success: false,
       error: 'Invalid Fields',
     };
+  }
+
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn('credentials', {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return {
+            success: false,
+            error: 'Invalid credentials',
+          };
+        default:
+          return {
+            success: false,
+            error: 'Somthing went wrong.',
+          };
+      }
+    }
+    throw error;
   }
 
   return {
--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,31 @@
-import GitHub from 'next-auth/providers/github';
 import type { NextAuthConfig } from 'next-auth';
-import Google from 'next-auth/providers/google';
+import Credentials from 'next-auth/providers/credentials';
+import { SignInSchema } from '@/schemas';
+import { getUserByEmail } from '@/data/user';
+import bcrypt from 'bcryptjs';
 
-export default { providers: [GitHub, Google] } satisfies NextAuthConfig;
+export default {
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = SignInSchema.safeParse(credentials);
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await getUserByEmail(email);
+          // If the user is loggedin by github/google provider and tries to access the loginpage, then since they have password they will return null
+          if (!user || !user.password) {
+            return null;
+          }
+
+          const passwordMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordMatch) {
+            return user;
+          }
+        }
+        return null;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
